712a64ec14a3c3209d43d1490fd5b449ae6e0bde
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-02-03 11:41:21 +0100

    03/02/2025 11:41

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+//----------------------Buttons-------------------------------------------
+
 const btnAboutUs = document.getElementById("btn-about-us");
 const btnContact = document.getElementById("button-contact");
 
@@ -8,3 +10,25 @@ btnAboutUs.addEventListener("click", () => {
 btnContact.addEventListener("click", () => {
   window.location.href = "./index.html#contact";
 });
+
+//----------------------Gallery-------------------------------------------
+
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 860;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 860;
+});
+
+// scrollContainer.addEventListener("wheel", (evt) => {
+//   //   evt.preventDefault();
+//   //   scrollContainer.scrollLeft += evt.deltaY;
+//   //   scrollContainer.style.scrollBehavior = "auto";
+// });
