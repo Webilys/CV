628fcb9ef3b333b294dc797414bfc7eac12999fa
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-02-05 06:12:39 +0100

    05/02/2025 06:12

---
+++ includes/a-propos.php
@@ -0,0 +1,31 @@
+<?php
+
+echo '
+<section id="a-propos">
+      <h2>À propos</h2>
+      <p>
+        Je suis Priscilla, une développeuse web fullstack passionnée et
+        entrepreneure dynamique. Fondatrice de Webilys, j\'accompagne mes
+        clients dans la création de solutions numériques sur mesure, alliant
+        innovation, performance et expérience utilisateur. <br /><br />
+        Mon expertise couvre un large éventail de technologies, allant de
+        HTML, CSS, JavaScript à des frameworks avancés comme React et
+        TypeScript. Je maîtrise également Git/GitHub, ainsi que des
+        plateformes CMS telles que WordPress. Grâce à cette polyvalence, je
+        conçois des interfaces intuitives et esthétiques, optimisées pour
+        l’ergonomie et l’accessibilité.
+        <br /><br />
+        Au-delà du développement, je combine design et ingénierie pour
+        proposer des expériences numériques uniques. Mon savoir-faire en SEO
+        et publicité en ligne me permet d’assurer une visibilité optimale
+        aux projets que je réalise.
+        <br /><br />
+        Toujours en veille technologique, je suis en quête d’apprentissage
+        continu et de nouveaux défis. Mon objectif est d’intégrer une équipe
+        dynamique ou de collaborer sur des projets ambitieux, où je pourrai
+        apporter ma créativité, mon expertise et mon engagement pour
+        façonner le web de demain.
+      </p>
+    </section>'
+
+    ?>
---
+++ includes/hero.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '
+<section class="hero">
+          <div>
+            <h1>Priscilla MEZOUAR</h1>
+            <p>Développeuse web fullstack</p>
+            <button class="buttonDegrade" id="btn-about-us">
+              <i class="bi bi-github" style="font-size:1.4rem;"></i> Accéder à mon GitHub
+            </button>
+          </div>
+        </section>'
+    ?>
