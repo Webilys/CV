e79f000575e46f625d345404493d2f61f19a2f2a
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-02-06 17:22:49 +0100

    06/02/2025 17:22

--- includes/skills.php
+++ includes/skills.php
@@ -1,120 +1,80 @@
 <?php
 echo '
-  <section id="competences" class="bsb-skill-2">
-    <h2>Skills</h2>
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-10 col-lg-8">
+<section id="competences" class="bsb-skill-2">
+  <h2>Skills</h2>
+  <div class="container overflow-hidden">
+    <div class="row gy-3 gy-lg-4">
+      <div class="col-12 col-lg-6">
+        <div class="card">
+          <div class="card-body p-4 p-xxl-5">
+            <h3 class="fw-bold mb-2">Frontend</h3>
+            <ul>
+              <li>Langages & Préprocesseurs : HTML, CSS, Sass</li>
+              <li>Frameworks & Bibliothèques : JavaScript, TypeScript, React, jQuery, Bootstrap</li>
+              <li>Outils & Build Tools : Webpack, Vite, ESLint, Prettier</li>
+            </ul>
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="container overflow-hidden">
-      <div class="row gy-3 gy-lg-4">
-        <div class="col-12 col-lg-6">
-          <div class="card">
-            <div class="card-body p-4 p-xxl-5">
-              <div class="row align-items-center">
-                <div class="col-12 skill-title">
-                  <h3 class="fw-bold mb-2">Frontend</h3>
-                  <ul>
-                    <li>
-                      Langages & Préprocesseurs : HTML, CSS, Sass
-                    </li>
-                    <li> Frameworks & Bibliothèques : JavaScript, TypeScript, React, jQuery, Bootstrap
-                    </li>
-                    <li> Outils & Build Tools : Webpack, Vite, ESLint, Prettier</li>
-                  </ul>
-                </div>
-                <div class="col-12 skill-progress">
-                  <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="100"
-                      aria-valuemin="0" aria-valuemax="100">100%</div>
-                  </div>
-                </div>
-              </div>
+      <div class="col-12 col-lg-6">
+        <div class="card">
+          <div class="card-body p-4 p-xxl-5">
+            <h3 class="fw-bold mb-2">Backend</h3>
+            <ul>
+              <li>Langages & Environnements : PHP</li>
+              <li>Frameworks : Symfony</li>
+              <li>Bases de données : MySQL, MariaDB</li>
+              <li>API & Protocoles : REST API</li>
+              <li>Outils DevOps & Serveurs : Netlify</li>
+            </ul>
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">60%</div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-lg-6">
-          <div class="card">
-            <div class="card-body p-4 p-xxl-5">
-              <div class="row align-items-center">
-                <div class="col-12 skill-title">
-                  <h3 class="fw-bold mb-2">Backend</h3>
-                  <ul>
-                    <li>Langages & Environnements : PHP</li>
-                    <li>Frameworks : Symfony</li>
-                    <li>Bases de données : MySQL, MariaDB
-                    </li>
-                    <li>API & Protocoles : REST API
-                    </li>
-                    <li>Outils DevOps & Serveurs : Netlify</li>
-                  </ul>
-                </div>
-                <div class="col-12 skill-progress">
-                  <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="100"
-                      aria-valuemin="0" aria-valuemax="100">100%
-                    </div>
-                  </div>
-                </div>
-              </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="card">
+          <div class="card-body p-4 p-xxl-5">
+            <h3 class="fw-bold mb-2">CMS & E-commerce</h3>
+            <ul>
+              <li>WordPress</li>
+              <li>WooCommerce</li>
+              <li>Prestashop</li>
+              <li>Shopify</li>
+              <li>Wix</li>
+              <li>Weebly</li>
+            </ul>
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-lg-6">
-          <div class="card">
-            <div class="card-body p-4 p-xxl-5">
-              <div class="row align-items-center">
-                <div class="col-12 col-md-7 skill-title">
-                  <h3 class="fw-bold mb-2">CMS & E-commerce</h3>
-                  <ul>
-                    <li>WordPress</li>
-                    <li>WooCommerce</li>
-                    <li>Prestashop</li>
-                    <li>Shopify</li>
-                    <li>Wix</li>
-                    <li>Weebly</li>
-                  </ul>
-                </div>
-                <div class="col-12 col-md-5 skill-progress">
-                  <div class="progress">
-                    <div class="progress-bar progress-bar-width-1" role="progressbar" aria-label="Bootstrap"
-                      aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-lg-6">
-          <div class="card">
-            <div class="card-body p-4 p-xxl-5">
-              <div class="row align-items-center">
-                <div class="col-12 col-md-7 skill-title">
-                  <h3 class="fw-bold mb-2">Outils & autres</h3>
-                  <ul>
-                    <li>Gestion de versions : Git, GitHub, GitLab</li>
-                    <li>Cloud & Hébergement : Google Cloud, Ionos 1&1, OVH</li>
-                    <li>Collaboration : Trello</li>
-                    <li>Outils de développement : Google Suite, VS Code</li>
-                    <li>SEO : Lighthouse, Google Search Console, PageSpeed Insights</li>
-                    <li>Création & Génération de Contenu : Canva, Adobe Suite, Figma, ChatGPT, Ideogram, Midjourney
-                    </li>
-                    <li>Automatisation & Productivité : Zapier</li>
-                  </ul>
-                </div>
-                <div class="col-12 col-md-5 skill-progress">
-                  <div class="progress">
-                    <div class="progress-bar progress-bar-width-1" role="progressbar" aria-label="Bootstrap"
-                      aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">95%</div>
-                  </div>
-                </div>
-              </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="card">
+          <div class="card-body p-4 p-xxl-5">
+            <h3 class="fw-bold mb-2">Outils & autres</h3>
+            <ul>
+              <li>Gestion de versions : Git, GitHub, GitLab</li>
+              <li>Cloud & Hébergement : Google Cloud, Ionos 1&1, OVH</li>
+              <li>Collaboration : Trello</li>
+              <li>Outils de développement : Google Suite, VS Code</li>
+              <li>SEO : Lighthouse, Google Search Console, PageSpeed Insights</li>
+              <li>Création & Génération de Contenu : Canva, Adobe Suite, Figma, ChatGPT, Ideogram, Midjourney</li>
+              <li>Automatisation & Productivité : Zapier</li>
+            </ul>
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width: 95%;" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">95%</div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
+
 '; ?>
