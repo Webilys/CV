4608d0e08935806788adfe91f57c0880cbf7d455
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-02-07 22:12:55 +0100

    correction

--- includes/links-head.php
+++ includes/links-head.php
@@ -1,7 +1,6 @@
 <?php
 echo '
 <link rel="shortcut icon" href="./assets/img/icon.ico" />
-  <link rel="icon" href="./assets/img/icon.ico" />
   <link rel="stylesheet" href="./assets/css/index.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
