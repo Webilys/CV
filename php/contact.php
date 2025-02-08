6f4abcd6383cd587238b5de14f42355b6b41fc81
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-02-08 17:50:57 +0100

    correction

--- php/contact.php
+++ php/contact.php
@@ -1,6 +1,4 @@
 <?php
-header("Content-Type: application/json; charset=UTF-8");
-
 $array = [
     "firstname" => "",
     "name" => "",
@@ -15,14 +13,14 @@ $array = [
     "isSucces" => false
 ];
 
-$emailTo = "contact@webilys.fr";
+$emailTo = "contact@webilys.fr"; // Email de réception
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $array["firstname"] = verifyInput($_POST['firstname'] ?? '');
-    $array["name"] = verifyInput($_POST['name'] ?? '');
-    $array["email"] = verifyInput($_POST['email'] ?? '');
-    $array["telephone"] = verifyInput($_POST['telephone'] ?? '');
-    $array["message"] = verifyInput($_POST['message'] ?? '');
+    $array["firstname"] = verifyInput($_POST['firstname']);
+    $array["name"] = verifyInput($_POST['name']);
+    $array["email"] = verifyInput($_POST['email']);
+    $array["telephone"] = verifyInput($_POST['telephone']);
+    $array["message"] = verifyInput($_POST['message']);
     $array["isSucces"] = true;
     $emailText = "";
 
@@ -47,11 +45,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $emailText .= "Email : {$array["email"]}\n";
     }
 
-    if (!empty($array["telephone"]) && !isPhone($array["telephone"])) {
+    if (!isPhone($array["telephone"])) {
         $array["telephoneError"] = "Veuillez entrer un numéro de téléphone valide";
         $array["isSucces"] = false;
-    } else if (!empty($array["telephone"])) {
-        $emailText .= "Téléphone : {$array["telephone"]}\n";
+    } else {
+        $emailText .= "Tel. : {$array["telephone"]}\n";
     }
 
     if (empty($array["message"])) {
@@ -62,19 +60,23 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     }
 
     if ($array["isSucces"]) {
-        $headers = "From: {$array["firstname"]} {$array["name"]} <{$array["email"]}>\r\n";
+        // Utilisation d'une adresse email du même domaine pour éviter le blocage
+        $fromEmail = "contact@webilys.fr"; // Remplace par une adresse email réelle du serveur
+        $headers = "From: Webilys <" . $fromEmail . ">\r\n";
         $headers .= "Reply-To: {$array["email"]}\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-        mail($emailTo, "Formulaire de contact CV Priscilla Mezouar", $emailText, $headers);
+        mail($emailTo, "Nouveau message de contact", $emailText, $headers);
     }
 
     echo json_encode($array);
 }
 
+// Fonctions de validation
 function isPhone($var)
 {
-    return preg_match("/^[0-9 .]+$/", $var);
+    return preg_match("/^[0-9 .]*$/", $var);
 }
 
 function isEmail($var)
@@ -84,6 +86,9 @@ function isEmail($var)
 
 function verifyInput($var)
 {
-    return htmlspecialchars(stripslashes(trim($var)));
+    $var = trim($var);
+    $var = stripslashes($var);
+    $var = htmlspecialchars($var);
+    return $var;
 }
 ?>
